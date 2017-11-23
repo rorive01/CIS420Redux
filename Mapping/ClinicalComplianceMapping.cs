654ecc6a59c84cf91b551299dcbe90deac98383d
rorive01 <raymondrivera47@gmail.com>
2017-11-23 14:11:09 -0500

    Add files via upload
    Mapping Files

---
+++ Mapping/ClinicalComplianceMapping.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using CIS420Redux.Models;
+
+namespace CIS420Redux.Mapping
+{
+    public class ClinicalComplianceMapping : EntityTypeConfiguration<ClincalCompliance>
+    {
+        public ClinicalComplianceMapping()
+        {
+            
+            Property(p => p.DocumentId).IsOptional();
+            
+            //HasKey(p => p.ID);
+            //Property(p => p.Type).IsRequired();
+            Property(p => p.ExpirationDate).IsOptional();
+            //Property(p => p.IsExpired).IsOptional();
+            //Property(p => p.IsCompliant).IsOptional();
+            //Property(p => p.StudentId).IsRequired();                        
+
+
+            //HasRequired(p => p.Student);
+
+        }
+
+    }
+}
